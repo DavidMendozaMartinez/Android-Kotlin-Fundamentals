96358d8af5c78debb9895a17fa7b985f0f431211
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-07-20 16:44:19 +0200

    Lesson 1 - Coding challenge: Add a second button to MainActivity. Add 1 to the current value of the result text when the button is clicked

--- lesson_1/DiceRoller/app/src/main/java/com/davidmendozamartinez/diceroller/MainActivity.kt
+++ lesson_1/DiceRoller/app/src/main/java/com/davidmendozamartinez/diceroller/MainActivity.kt
@@ -7,18 +7,30 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var resultText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        resultText = findViewById(R.id.result_text)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
         val randomInt = (1..6).random()
-
-        val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        resultText.text = when (val resultInt = resultText.text.toString().toIntOrNull()) {
+            null -> "1"
+            in 1..5 -> "${resultInt + 1}"
+            else -> "6"
+        }
     }
 }
