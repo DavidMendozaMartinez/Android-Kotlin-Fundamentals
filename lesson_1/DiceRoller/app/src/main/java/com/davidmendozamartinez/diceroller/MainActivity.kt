7c25f7f9d6336933342356942227a5ee90e423b1
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-07-20 16:55:28 +0200

    Lesson 1 - Coding challenge: Add a second ImageView to MainActivity to display an image of the second dice result

--- lesson_1/DiceRoller/app/src/main/java/com/davidmendozamartinez/diceroller/MainActivity.kt
+++ lesson_1/DiceRoller/app/src/main/java/com/davidmendozamartinez/diceroller/MainActivity.kt
@@ -7,26 +7,32 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var diceImage: ImageView
+    private lateinit var diceImage1: ImageView
+    private lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDices() }
     }
 
-    private fun rollDice() {
-        val drawableResource = when ((1..6).random()) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        diceImage.setImageResource(drawableResource)
+    private fun rollDices() {
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
     }
+
+    private fun getRandomDiceImage(): Int =
+            when ((1..6).random()) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
 }
