0b6f6c5209fc7637fa543b4df8fa4062f36d0cf5
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-08-13 00:34:59 +0200

    Lesson 7 - Create the DataItem class. Create the TextViewHolder for the Header. Add a header to the RecyclerView

--- lesson_7/TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ lesson_7/TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -77,14 +77,21 @@ class SleepTrackerFragment : Fragment() {
         val adapter = SleepNightAdapter(SleepNightListener { nightId ->
             sleepTrackerViewModel.onSleepNightClicked(nightId)
         })
-        val manager = GridLayoutManager(activity, 3)
         binding.sleepList.adapter = adapter
+
+        val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) = when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
         binding.sleepList.layoutManager = manager
 
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.submitList(it)
+                adapter.addHeaderAndSubmitList(it)
             }
         })
 
